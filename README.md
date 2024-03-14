a70d7e7cb50274a4e784ecff822a7ed6e2214e53
xrando <2837341L@student.gla.ac.uk>
2024-03-14 10:51:23 +0800

    updates
    updates

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # FoodRecognitionML
+
+# Steps to deploy on aws ec2
+1. Create an EC2 instance
+2. Install git on the instance
+3. Clone the repository
+4. Install python3
+5. Install pip3
+6. pip install tensorflow-cpu==2.7.0
+7. pip install protobuf==3.20
+8. pip install flask
+9. pip install pillow
+
+# Steps to run the application
+flask run --host=0.0.0.0
+
--- app.py
+++ app.py
@@ -2,25 +2,34 @@ from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
-import io
+import os
 
 app = Flask(__name__)
 
 # Load the trained model
-model = load_model('food_classification_model.h5')
+model = load_model('food_classification_model.h5', compile=False)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
 
 @app.route('/classify-image', methods=['POST'])
 def predict():
-    file = request.files['file']
-    img = image.load_img(io.BytesIO(file.stream.read()), target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
+    if request.method == 'POST':
+        file = request.files['file']
+        file_path = '/tmp/uploaded_image.jpg'
+        file.save(file_path)
+        img = image.load_img(file_path, target_size=(224, 224))
+        os.remove(file_path)  # Remove the temporary file
 
-    # Perform prediction
-    prediction = model.predict(img_array)
-    result = "Non-Food" if prediction[0][0] >= 0.5 else "Food"
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0) / 255.0
 
-    return jsonify({"prediction": result})
+        # Perform prediction
+        prediction = model.predict(img_array)
+        result = "Non-Food" if prediction[0][0] >= 0.5 else "Food"
+
+        return jsonify({"prediction": result})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
